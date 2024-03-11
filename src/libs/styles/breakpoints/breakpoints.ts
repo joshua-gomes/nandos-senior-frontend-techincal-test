efec3394097159470c8f26c3b8e5f81e20b11052
Joshimoto <joshuagomes97@gmail.com>
2024-03-11 18:30:34 +0000

    Added breakpoints and spacing constants

---
+++ src/libs/styles/breakpoints/breakpoints.ts
@@ -0,0 +1,4 @@
+export const breakpoints = {
+  mobile: 592,
+  desktop: 1440,
+};
---
+++ src/libs/styles/spacing/spacing.css.ts
@@ -0,0 +1,7 @@
+import { convertPixelValueToRem } from "@libs/helpers/convertPixelValueToRem";
+
+export const spacing = {
+  small: convertPixelValueToRem(16),
+  medium: convertPixelValueToRem(24),
+  large: convertPixelValueToRem(32),
+};
