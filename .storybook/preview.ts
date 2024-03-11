2a5aa78889a4f8e626799ab89f4bddc731bdb00c
Joshimoto <joshuagomes97@gmail.com>
2024-03-11 17:14:13 +0000

    Setup styled-components with storybook and configured the Barlow-Regular font for the project

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,25 @@
 import type { Preview } from "@storybook/react";
 
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Barlow-Regular';
+    src: url("./Barlow-Regular.ttf") format('truetype'),
+  }
+
+  body {
+    font-family: "Barlow-Regular", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+`;
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles, // Adds your GlobalStyle component to all stories
+  }),
+];
+
 const preview: Preview = {
   parameters: {
     controls: {
