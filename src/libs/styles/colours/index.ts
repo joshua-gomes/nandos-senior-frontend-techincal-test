895f1f891f3554206b5d3c59461c34affd597244
Joshimoto <joshuagomes97@gmail.com>
2024-03-11 17:39:36 +0000

    Added colours constants and added aliases to tsconfig.json

---
+++ src/libs/styles/colours/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Colour names are decided by following matching color names on:
+ * https://www.color-name.com
+ */
+
+export const colours = {
+  black: "#000000",
+  darkLiver: "#505050",
+  mexicanPink: "#E6007D",
+  milk: "#FFFEF6",
+  shockingPink: "#FF00B4",
+};
